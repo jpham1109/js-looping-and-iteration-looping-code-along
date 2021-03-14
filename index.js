c2db44740524627fdaad705ea5ae4ceb2cc024df
Jamie H Pham <jpham1109@gmail.com>
2021-03-13 20:47:11 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    const messages = [];
+    let i = 0;
+    while (i < array.length) {
+    messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    i++;
+    }
+    return messages;
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    };
+}
+
